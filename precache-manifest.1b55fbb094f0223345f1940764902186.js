89e7753fd7ffc53316cc3262bc7918a97366d83d
Anuar Talipov <anuartb@gmail.com>
2019-04-26 22:13:08 +0900

    Updates

---
+++ precache-manifest.1b55fbb094f0223345f1940764902186.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7f6f542d615189fc5aabc58fc1b9570",
+    "url": "/atak-ui/index.html"
+  },
+  {
+    "revision": "b9d2b034290a270f7698",
+    "url": "/atak-ui/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/atak-ui/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "b9d2b034290a270f7698",
+    "url": "/atak-ui/static/js/main.1a0fc8d9.chunk.js"
+  },
+  {
+    "revision": "79b402231944b99f60cb",
+    "url": "/atak-ui/static/js/runtime~main.d933a830.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/atak-ui/static/media/logo.5d5d9eef.svg"
+  }
+]);
