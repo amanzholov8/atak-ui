1dba5416b39f093cf5018826bcd5c5444b8354b1
Anuar Talipov <anuartb@gmail.com>
2019-04-29 14:54:48 +0900

    Navbar init in React

--- choreo-creator/src/App.js
+++ choreo-creator/src/App.js
@@ -2,8 +2,33 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlay, faPause, faRecycle } from '@fortawesome/free-solid-svg-icons'
+import Navbar from 'react-bootstrap/Navbar';
+
+library.add(faPlay, faPause);
+
+class MyNavigationBar extends React.Component {
+  render(){
+    return (
+      <Navbar>
+        <Navbar.Brand href="#home">Navbar with text</Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          <Navbar.Text>
+            Signed in as: <a href="#login">Mark Otto</a>
+          </Navbar.Text>
+        </Navbar.Collapse>
+      </Navbar>
+    );
+  }
+}
+
 function App() {
   return (
+    <div>
+    <MyNavigationBar />
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -19,6 +44,7 @@ function App() {
           Learn React
         </a>
       </header>
+    </div>
     </div>
   );
 }
