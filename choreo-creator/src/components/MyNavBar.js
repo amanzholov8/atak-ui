5c7559090e1782ff0a4437fb5a359944f8a4824c
amanzholov8 <alim.amanzholov@gmail.com>
2019-05-06 20:39:02 +0900

    added some buttons

--- choreo-creator/src/components/MyNavBar.js
+++ choreo-creator/src/components/MyNavBar.js
@@ -1,18 +1,39 @@
 import React from 'react';
 
 import Navbar from 'react-bootstrap/Navbar';
-
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import Nav from 'react-bootstrap/Nav';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Button from 'react-bootstrap/Button';
 
 class MyNavBar extends React.Component {
     render(){
         return (
         <Navbar>
             <Navbar.Toggle children={<h1>WTF</h1>} />
-            <Navbar.Collapse className="justify-content-end">
-            <Navbar.Text>
-                Signed in as: <a href="#login">Mark Otto</a>
-            </Navbar.Text>
+            <Navbar.Collapse className="justify-content-center">
+            <Nav className="justify-content-between">
+                <Nav.Item>
+                    <Button variant='primary'>
+                        <FontAwesomeIcon icon='play' size='3x'/>
+                    </Button>
+                </Nav.Item>
+                <Nav.Item>
+                  <Button variant='primary'>
+                        <FontAwesomeIcon icon='pause' size='3x'/>
+                    </Button>
+                    
+                </Nav.Item>
+                <Button variant='primary'>
+                        LOOP
+                    </Button>
+                <Nav.Item>
+
+                <Nav.Item>
+                    <Button variant='primary'> HELP </Button>
+                </Nav.Item>
+
+                </Nav.Item>
+            </Nav>
             </Navbar.Collapse>
         </Navbar>
         );
