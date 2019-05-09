7de8825f546ac3a5096e23ee7d0ba2ff636e090c
Temirlan Myrzakhmetov <temirlanmirzakhmetov@gmail.com>
2019-05-09 21:25:38 +0900

    Add reducers.js

---
+++ choreo-creator/src/reducers.js
@@ -0,0 +1,17 @@
+import {ACTION_ADD_PHOTO} from "SDKLJSDLKFJSDLKFJ";
+
+const initialState = {
+	beats: [Array(7).fill({})] //7 is the total number of beats
+}
+
+function getBeatIndex(beatSelected) {
+	return (((beatselected.bar - 1) * 4) + (beatselected.beat - 1)) //4 is the number of beats in each bar
+}
+
+export const rootReducer = (state = InitialState, action) => {
+	switch (action.type) {
+		case ACTION_ADD_PHOTO:
+			return { ...state, beats[getBeatIndex(action.beatselected)].media: [...beats[getBeatIndex(action.beatselected)].media, action.src]};
+	}
+	return state
+}
