fb0379805ee9ee869f1e81746ebba5bbf19f18a4
Anuar Talipov <anuartb@gmail.com>
2019-05-13 17:17:22 +0900

    Updates

--- choreo-creator/src/actions.js
+++
@@ -1,9 +0,0 @@
-/*import {ACTION_ADD_PHOTO} from "./index";
-
-export const addPhoto = (beatselected, src) => {
-	return {
-		type: ACTION_ADD_PHOTO,
-		beatselected: beatselected,
-		src: src
-	};
-};*/
